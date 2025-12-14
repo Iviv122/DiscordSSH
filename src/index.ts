645e82f0861202917a79b06eee199210f1e339e2
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-12-14 20:41:24 +0100

    bot logs in

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
-import { Client, IntentsBitField } from "discord.js";
+// idfk why
+import "dotenv/config";
+import { defaultConfig } from "@/config";
+import { ActivityType, Client, IntentsBitField } from "discord.js";
 
-const client = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-  ],
-});
+if (!!!defaultConfig.token) {
+  throw "No token set";
+}
+
+try {
+  const client = new Client({
+    allowedMentions: {
+      parse: ["users", "roles"],
+      repliedUser: false,
+    },
+    intents: [
+      IntentsBitField.Flags.Guilds,
+      IntentsBitField.Flags.GuildMembers,
+      IntentsBitField.Flags.GuildMessages,
+      IntentsBitField.Flags.MessageContent,
+    ],
+  });
+
+  client.on("clientReady", (c) => {
+    console.log(`The bot ${c.user.tag} is ready to serve you!`);
+  });
+
+  client.on("messageCreate", (message) => {
+    console.log("Got new message:", message.content);
+    if (message.content === "run") {
+      message.reply("HEY");
+    }
+  });
+
+  console.log("Logging in");
+  client.login(defaultConfig.token);
+  console.log("Succesfull login");
+
+  client.user?.setActivity("Serving SSH", { type: ActivityType.Custom });
+} catch (error) {
+  console.log("Error starting bot: ", error);
+  throw error;
+}
