84fcfd46ad521ef748466fdc1df80b7c70c94706
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-12-14 20:07:30 +0100

    More config

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Client, IntentsBitField } from "discord.js";
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
